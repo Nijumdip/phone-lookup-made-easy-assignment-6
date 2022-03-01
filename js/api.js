8f6772ae8ebde457ceb9ba94d7823597722ee161
Nijumdip <nijumdip019911@gmail.com>
2022-03-01 10:59:50 +0600

    add display phone

--- js/api.js
+++ js/api.js
@@ -1,52 +1,74 @@
 const mainDiv = document.getElementById('main-one');
-const searchPhone = () => {
-    const searchFeild = document.getElementById('search-feild');
-    const error = document.getElementById('error');
-    const searchFeildText = searchFeild.value;
-    console.log(searchFeildText);
-    if (searchFeildText == '') {
-        error.innerText = 'Please write something  to display ';
-        searchFeild.value = '';
-        mainDiv.innerHTML = '';
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const loadData = () => {
-    fetch('https://openapi.programming-hero.com/api/phones')
-        .then(res => res.json())
-        .then(data => displayPhone(data.data[0]));
+    const searchPhone = () => {
+        const searchFeild = document.getElementById('search-feild');
+        const error = document.getElementById('error');
+        const searchFeildText = searchFeild.value;
+        // console.log(searchFeildText);
+        if (searchFeildText == '') {
+            error.innerText = 'Please write something  to display ';
+            searchFeild.value = '';
+            mainDiv.innerHTML = '';
+        }
+        else {
+            const url = `https://openapi.programming-hero.com/api/phones?search=${searchFeildText}`
+            fetch(url)
+                .then(res => res.json())
+                .then(data => displayPhone(data.data));
+            searchFeild.value = '';
+            error.innerText = '';
+            mainDiv.innerHTML = '';
+        }
+    };
+    const displayPhone = phones => {
+        // console.log(phones);
+        /* if (phones.length == 0) {
+            error.innerText = 'show no result found';
+        }
+        else { }*/
+        for (const phone of phones) {
+            console.log(phone);
+            const div = document.createElement('div');               
+            div.classList.add("col-lg-4", 'col-sm-1');
+            div.classList.add("mb-5", 'mt-5');
+            div.innerHTML = `
+                <div class="card p-2 rounded" style="width: 18rem;">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">${phone.brand}</p>
+                            <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">See Details</button>
+                    </div>
+                </div>
+                `;
+            mainDiv.appendChild(div);
+        };
+    };
+    const loadPhoneDetail = phoneId => {
+        console.log(phoneId);
+       /*  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${phoneId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayMealDetail(data.meals[0])) */
+    };
     
-}
-loadData();
-const displayPhone = phones => {
-    console.log(phones);
-    const mainOne = document.getElementById('main-one');
-} */
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
