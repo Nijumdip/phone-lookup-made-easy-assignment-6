da2c6b8878962f9ef587e147c514b4b5931c511a
Nijumdip <nijumdip019911@gmail.com>
2022-03-01 09:01:51 +0600

    add error

--- js/api.js
+++ js/api.js
@@ -1,1 +1,52 @@
-console.log('added');
+const mainDiv = document.getElementById('main-one');
+const searchPhone = () => {
+    const searchFeild = document.getElementById('search-feild');
+    const error = document.getElementById('error');
+    const searchFeildText = searchFeild.value;
+    console.log(searchFeildText);
+    if (searchFeildText == '') {
+        error.innerText = 'Please write something  to display ';
+        searchFeild.value = '';
+        mainDiv.innerHTML = '';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const loadData = () => {
+    fetch('https://openapi.programming-hero.com/api/phones')
+        .then(res => res.json())
+        .then(data => displayPhone(data.data[0]));
+    
+}
+loadData();
+const displayPhone = phones => {
+    console.log(phones);
+    const mainOne = document.getElementById('main-one');
+} */
